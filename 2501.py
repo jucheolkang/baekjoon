c7713cc996c2848f946747e7bfaf9949b282ad07
jucheolkang <jucheol9877@naver.com>
2023-09-16 13:03:52 +0900

    update 2501_약수구하기

--- 2501.py
+++ 2501.py
@@ -1,7 +1,12 @@
 a,b = map(int, input().split())
 
-for i in range(a):
+for i in range(1,a+1):
     if a % i == 0:
-        b-=1
-        if b == 1:
+        b -=1
+        if b == 0:
             print(i)
+            break
+if b != 0:
+    print(0)
+
+    
