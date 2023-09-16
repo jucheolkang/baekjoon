82189e0e5501b625ce4dbec7d0830a161c600a2e
jucheolkang <jucheol9877@naver.com>
2023-09-16 14:47:12 +0900

    solving 2503_숫자야구

---
+++ 2503_숫자야구.py
@@ -0,0 +1,10 @@
+roof = int(input())
+nums = []
+for _ in range(roof) : 
+    num, s, b = map(int, input().split())
+    num = str
+    nums.append(num)
+print(nums)
+for i in nums:
+    for j in i:
+        print(j)
