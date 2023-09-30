cce134c23c11e4c833132db4d821a0ffd9e6cbb0
jucheolkang <jucheol9877@naver.com>
2023-09-30 23:35:29 +0900

    9/30

--- 9093_단어뒤집기.py
+++ 9093_단어뒤집기.py
@@ -2,6 +2,7 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 s = [list(input().split()) for i in range(n)]
+
 for i in range(n):
     for j in s[i]:
         print(''.join(reversed(list(j))), end=" ")
